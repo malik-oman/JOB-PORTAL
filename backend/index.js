93903f51bc724093c47bd52b47577ec5febc9ed3
Malik-Oman <malikoman258@gmail.com>
2026-03-28 22:13:56 -0700

    initial commit with frontend and backend job portal setup

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { connectDB } from './config/connectDB.js';
+dotenv.config()
+
+const app = express()
+
+const allowedOrigins = ["http://localhost:5173"];
+
+//midlleware=======================
+app.use(express.json())
+app.use(cors({origin: allowedOrigins, credentials:true}));
+app.use(cookieParser());
+
+// CONNECTION DATABASE=======================
+connectDB();
+
+//API ENDPOINTS====================
+app.get("/",(req,res)=>{
+  res.send("helo")
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,() => {
+  console.log(`SERVER IS RUNING ON PORT ${PORT}`)
+})
